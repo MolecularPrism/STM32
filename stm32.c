3a2339cc830d0cf210a002404040b83d708bcb6d
MolecularPrism <54078316+MolecularPrism@users.noreply.github.com>
2023-04-07 18:08:02 -0700

    added identity byte distinguishing feature (NOT TESTED)

--- stm32.c
+++ stm32.c
@@ -8,6 +8,13 @@
 #define PERIOD 2
 #define JUNK 3
 
+//Identity 
+#define Speed_ID 1
+#define Temp_ID 2
+#define Volt_ID 3
+#define MISC_ID 4
+#define NOT_A_ID_BYTE 100
+
 
 int binaryToDecimal(char* binary)
 {
@@ -67,6 +74,23 @@ int check_nondec_symbol(char* buf){
 
 }
 
+int identity_byte(char* buf){
+	int value_type;
+	if(strcmp(buf, "01010011") == 0) { //if S (Speed)
+		value_type = Speed_ID;
+	}
+	else if(strcmp(buf, "01010100") == 0) { //if T (Temp)
+		value_type = Temp_ID;
+	}
+	else if(strcmp(buf, "01010110") == 0) { //if V (Voltage)
+		value_type = Volt_ID;
+	}
+	else{
+		value_type = NOT_A_ID_BYTE; //if junk value then do nothing (random value LOL)
+	}
+	return value_type;
+}
+
 char* ascii_to_decimal(char* buf){
 	char* decimal;
 
@@ -117,6 +141,9 @@ void loop() {
 	int is_decimal;
 	int check_symbol;
 	int pos = 0;
+	int temporary_value_identity;
+	int saved_value_identity;
+
 
 
 
@@ -125,33 +152,45 @@ void loop() {
   
 
     is_decimal = is_Decimal(buffer);
-	  //check what non-decimal symbol it is
-	  check_symbol = check_nondec_symbol(buffer);
+	//check what non-decimal symbol it is
+	check_symbol = check_nondec_symbol(buffer);
+	if(buffer)
+	temporary_value_identity = identity_byte(buffer); 
 
-	 
-	  if(is_decimal == 1){
+	if(temporary_value_identity != NOT_A_ID_BYTE){
+		saved_value_identity = temporary_value_identity;
+	}
+
+	
+		
+	if(is_decimal == 1){
 		//if it's decimal then convert ascii to decimal val
 		decimal_val = ascii_to_decimal(buffer);
-    //Serial.printf("current decimal digit: %s", decimal_val);
+		//Serial.printf("current decimal digit: %s", decimal_val);
 		strcat(data_value, decimal_val); //concatenate decimal digit to data arr
 
-	  }
-     //if it's not decimal, then check what kind of character it is
-	  else{
-		  
+	}
+	//if it's not decimal, then check what kind of character it is
+	else{
 
-		  //if it's a period then we concatenate to data_value
-		  if(check_symbol == PERIOD){
-			  strcat(data_value, ".");
-	  	}
+		//if it's a period then we concatenate to data_value
+		if(check_symbol == PERIOD){
+			strcat(data_value, ".");
+		}
 
-      //if delimiter byte, then we clear data_value
-		  if(check_symbol == DELIMITER){
-			  data_value[0] = '\0';
-      }
+		//if delimiter byte, then we clear data_value
+		if(check_symbol == DELIMITER){
+			//display data_value on LCD and the cursor location will depend on saved_value_identity
 
-		  //if it's a white space or some junk value, we just do nothing
-    }
+			data_value[0] = '\0'; //clear Data Value for new income value
+		}
+
+		//if it's a white space or some junk value, we just do nothing
+	}
+	
+
+	
+	
 
   
     
